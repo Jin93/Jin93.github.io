09ef7b5c39845aa58c3157ac569477e4e0517056
Jin Jin <jjin31@jhu.edu>
2021-07-07 11:22:40 -0400

    Updated

--- _pages/life.R
+++
@@ -1,8 +0,0 @@
-
-
-### Fun facts:
-
-- I love baking (check my instagram: [tianying.w](https://www.instagram.com/tianying.w/?hl=en)).  
-- I enjoy scuba diving and hold an Advanced Scuba Diving Certificate.
-
-
