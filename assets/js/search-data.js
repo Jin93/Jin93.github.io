41d05425c5969c42d9372e3f3b988db5e4f5e311
Jin93 <jin.jin@pennmedicine.upenn.edu>
2025-06-05 05:47:22 +0000

    Deploying to gh-pages from @ Jin93/Jin93.github.io@18d56407995656f7f581477b9891c7fb4ff5b130 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-computing",
           title: "Computing",
           description: "",
