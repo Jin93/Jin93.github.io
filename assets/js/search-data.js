37d30822c6fb05a7d4b28c373ec95ad623d062c1
Jin93 <jin.jin@pennmedicine.upenn.edu>
2025-06-05 06:51:50 +0000

    Deploying to gh-pages from @ Jin93/Jin93.github.io@f0f85ece65bbb9fda6455c526fc447e38c14a8d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,11 +412,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+            },},{id: "news-pennprs-version-1-0-0-is-now-available-online",
+          title: 'PennPRS version 1.0.0 is now available online.',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/announcement_PennPRS/";
             },},{id: "news-our-paper-on-mrle-mendelian-randomization-analysis-using-multiple-biomarkers-of-an-underlying-common-exposure-was-published-in-biostatistics",
           title: 'Our paper on MRLE: Mendelian randomization analysis using multiple biomarkers of an underlying...',
           description: "",
