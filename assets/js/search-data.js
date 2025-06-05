c79cde840231f2b7cd309a8057f2ab92040243e5
Jin93 <jin.jin@pennmedicine.upenn.edu>
2025-06-05 05:33:33 +0000

    Deploying to gh-pages from @ Jin93/Jin93.github.io@059fc095799bfe204c055762b68fad6af356f37b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-people",
-    title: "People",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
